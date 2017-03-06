3b85717c3a7ca4bf06c081230d97507d7d8c34e2
M. Simon Borg <msimonborg@gmail.com>
2017-03-06 10:04:00 -0500

    add scopes

---
+++ lib/lazy_record/scopes.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+module LazyRecord
+  # Add ActiveRecord-style scope macros to your classes. Allows chaining.
+  module Scopes
+    SCOPE_MODULE_NAME = :ScopeMethods
+
+    def my_scope(method_name, lambda)
+      if const_defined?(SCOPE_MODULE_NAME, _search_ancestors = false)
+        mod = const_get(SCOPE_MODULE_NAME)
+      else
+        mod = const_set(SCOPE_MODULE_NAME, Module.new)
+        extend mod
+      end
+
+      mod.module_eval do
+        send(:define_method, method_name, &lambda)
+      end
+    end
+  end
+end
