4c2f86b26a73dd3cc9c0f1c3eed9c3ebccbfb9b0
M. Simon Borg <msimonborg@gmail.com>
2017-03-06 10:28:18 -0500

    add dynamic_modules and attributes

---
+++ lib/lazy_record/attributes.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+module LazyRecord
+  # Add special attr_accessors that automatically add initialization options
+  # to your initialize method. Using lr_attr_accessor, you automatically get
+  # an #initialize method that takes setter options for each attribute and
+  # yields self to a block. If you want to add custom functionality to
+  # #initialize just call super.
+  module Attributes
+    ATTR_MODULE_NAME = :DynamicAttributes
+    
+    def self.included(klass)
+      klass.class_eval do
+        include DynamicModules
+      end
+    end
+
+    def my_attr_accessor(*names)
+      mod = get_or_set_and_include_mod(ATTR_MODULE_NAME)
+
+      mod.module_eval do
+        names.each do |name|
+          define_method(name) do
+            instance_variable_get('@' + name.to_s)
+          end
+
+          define_method("#{name}=") do |val|
+            instance_variable_set('@' + name.to_s, val)
+          end
+        end
+
+        define_method(:instance_attr_accessors) do
+          names.map(&:to_sym)
+        end
+
+        def initialize(opts = {})
+          instance_attr_accessors.each do |attr|
+            send("#{attr}=", opts[attr.to_sym])
+          end
+
+          yield self if block_given?
+        end
+
+        def instance_attrs_to_s
+          instance_attr_accessors.map do |attr|
+            value = send(attr)
+            attr_to_s = if value.is_a?(String)
+                          "\"#{value}\""
+                        elsif value.nil?
+                          'nil'
+                        else
+                          value
+                        end
+            "#{attr.to_s.delete(':')}: #{attr_to_s}"
+          end
+        end
+
+        private :instance_attr_accessors, :instance_attrs_to_s
+      end
+    end
+  end
+end
---
+++ lib/lazy_record/dynamic_modules.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+module LazyRecord
+  # Generate dynamic modules for dynamic methods created with #define_method,
+  # for insertion into inheritance chain. This allows you to make calls to
+  # super for these methods.
+  module DynamicModules
+    def get_or_set_and_include_mod(module_name)
+      if const_defined?(module_name, _search_ancestors = false)
+        mod = const_get(module_name)
+      else
+        mod = const_set(module_name, Module.new)
+        include mod
+        mod # return the Module for variable assignment.
+      end
+    end
+  end
+end
