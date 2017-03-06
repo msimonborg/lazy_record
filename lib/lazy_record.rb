bf510f83b3f455339c61715f4d64f7b3a2c8752b
M. Simon Borg <msimonborg@gmail.com>
2017-03-06 10:54:48 -0500

    add require statements

--- lib/lazy_record.rb
+++ lib/lazy_record.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 require 'lazy_record/version'
+require 'lazy_record/associations'
+require 'lazy_record/attributes'
+require 'lazy_record/base'
+require 'lazy_record/callbacks'
+require 'lazy_record/dynamic_modules'
+require 'lazy_record/methods'
+require 'lazy_record/scopes'
+require 'lazy_record/validations'
+
 # Namespace
 module LazyRecord; end
