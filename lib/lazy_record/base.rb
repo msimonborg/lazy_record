a87b81adaed3d546d1fe35408fa24f85aa6f9f39
M. Simon Borg <msimonborg@gmail.com>
2017-03-08 01:05:18 -0500

    add option to include module instead of inherit class

--- lib/lazy_record/base.rb
+++ lib/lazy_record/base.rb
@@ -7,61 +7,6 @@ module LazyRecord
   # method generating macros, scope methods and scope chaining, associations
   # with other objects, and validations. This is a WIP still in development.
   class Base
-    extend Scopes
-    extend Attributes
-    extend Associations
-    extend Callbacks
-    extend Validations
-    extend Methods
-    extend DynamicModules
-
-    class << self
-      attr_reader :all
-    end
-
-    attr_writer :id
-
-    def self.count
-      @all.count
-    end
-
-    def self.first
-      @all.first
-    end
-
-    def self.last
-      @all.last
-    end
-
-    def self.where(condition)
-      @all.where(condition)
-    end
-
-    def self.destroy_all
-      @all.send(:clear)
-    end
-
-    def initialize(_opts = {})
-      yield self if block_given?
-    end
-
-    def instance_attrs_to_s
-      []
-    end
-
-    def instance_attr_accessors
-      []
-    end
-
-    def id
-      @id.freeze
-    end
-
-    private :id=
-
-    def inspect
-      "#<#{self.class} id: #{id ? id : 'nil'}"\
-      "#{instance_attrs_to_s.unshift('').join(', ')}>"
-    end
+    include BaseModule
   end
 end
---
+++ lib/lazy_record/base_module.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+module LazyRecord
+  module BaseModule
+    def self.included(base)
+      base.extend(
+        ClassMethods,
+        Scopes,
+        Attributes,
+        Associations,
+        Callbacks,
+        Validations,
+        Methods,
+        DynamicModules
+      )
+    end
+    
+    attr_writer :id
+
+    def initialize(_opts = {})
+      yield self if block_given?
+    end
+
+    def instance_attrs_to_s
+      []
+    end
+
+    def instance_attr_accessors
+      []
+    end
+
+    def inspect
+      "#<#{self.class} id: #{id ? id : 'nil'}"\
+      "#{instance_attrs_to_s.unshift('').join(', ')}>"
+    end
+
+    def id
+      @id.freeze
+    end
+
+    private :id=
+
+    module ClassMethods
+      attr_reader :all
+
+      def count
+        @all.count
+      end
+
+      def first
+        @all.first
+      end
+
+      def last
+        @all.last
+      end
+
+      def where(condition)
+        @all.where(condition)
+      end
+
+      def destroy_all
+        @all.send(:clear)
+      end
+    end
+  end
+end
