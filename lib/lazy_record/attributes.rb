529a702471c3e2ad240efc1ecd434d6f07c02a83
M. Simon Borg <msimonborg@gmail.com>
2017-03-08 19:27:37 -0500

    get rid of unnecessary lambdas

--- lib/lazy_record/attributes.rb
+++ lib/lazy_record/attributes.rb
@@ -8,57 +8,49 @@ module LazyRecord
   module Attributes
     ATTR_MODULE_NAME = :DynamicAttributes
 
-    def define_setters_and_getters
-      lambda do |name|
-        define_method(name) do
-          instance_variable_get('@' + name.to_s)
-        end
+    def define_setters_and_getters(name)
+      define_method(name) do
+        instance_variable_get('@' + name.to_s)
+      end
 
-        define_method("#{name}=") do |val|
-          instance_variable_set('@' + name.to_s, val)
-        end
+      define_method("#{name}=") do |val|
+        instance_variable_set('@' + name.to_s, val)
       end
     end
 
     def define_initialize
-      lambda do
-        define_method(:initialize) do |opts = {}, &block|
-          instance_attr_accessors.each do |attr|
-            send("#{attr}=", opts[attr.to_sym])
-          end
-
-          block.call self if block
-          self
+      define_method(:initialize) do |opts = {}, &block|
+        instance_attr_accessors.each do |attr|
+          send("#{attr}=", opts[attr.to_sym])
         end
+
+        block.call self if block
+        self
       end
     end
 
     def define_instance_attrs_to_s
-      lambda do
-        define_method(:instance_attrs_to_s) do
-          instance_attr_accessors.map do |attr|
-            value = send(attr)
-            attr_to_s = if value.is_a?(String)
-                          "\"#{value}\""
-                        elsif value.nil?
-                          'nil'
-                        else
-                          value
-                        end
-            "#{attr.to_s.delete(':')}: #{attr_to_s}"
-          end
+      define_method(:instance_attrs_to_s) do
+        instance_attr_accessors.map do |attr|
+          value = send(attr)
+          attr_to_s = if value.is_a?(String)
+                        "\"#{value}\""
+                      elsif value.nil?
+                        'nil'
+                      else
+                        value
+                      end
+          "#{attr.to_s.delete(':')}: #{attr_to_s}"
         end
-        private :instance_attrs_to_s
       end
+      private :instance_attrs_to_s
     end
 
-    def define_instance_attr_accessors
-      lambda do |*names|
-        define_method(:instance_attr_accessors) do
-          names.map(&:to_sym)
-        end
-        private :instance_attr_accessors
+    def define_instance_attr_accessors(*names)
+      define_method(:instance_attr_accessors) do
+        names.map(&:to_sym)
       end
+      private :instance_attr_accessors
     end
 
 
@@ -66,10 +58,10 @@ module LazyRecord
       include mod = get_or_set_mod(ATTR_MODULE_NAME)
       mod.extend(Attributes)
       mod.module_eval do
-        names.each { |name| define_setters_and_getters.call(name) }
-        define_instance_attr_accessors.call(*names)
-        define_initialize.call
-        define_instance_attrs_to_s.call
+        names.each { |name| define_setters_and_getters(name) }
+        define_instance_attr_accessors(*names)
+        define_initialize
+        define_instance_attrs_to_s
       end
     end
   end
