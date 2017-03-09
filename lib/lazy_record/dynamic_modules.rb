7498a550db23390a94d8cd6232ffa24c638607e3
M. Simon Borg <msimonborg@gmail.com>
2017-03-08 19:21:42 -0500

    DRY dynamic modules

--- lib/lazy_record/dynamic_modules.rb
+++ lib/lazy_record/dynamic_modules.rb
@@ -4,21 +4,11 @@ module LazyRecord
   # for insertion into inheritance chain. This allows you to make calls to
   # super for these methods.
   module DynamicModules
-    def get_or_set_and_include_mod(module_name)
-      if const_defined?(module_name, _search_ancestors = false)
-        const_get(module_name)
-      else
-        mod = const_set(module_name, Module.new)
-        include mod
-        mod # return the Module for variable assignment.
-      end
-    end
-
     def get_or_set_mod(module_name)
       if const_defined?(module_name, _search_ancestors = false)
         const_get(module_name)
       else
-        mod = const_set(module_name, Module.new)
+        const_set(module_name, Module.new)
       end
     end
   end
