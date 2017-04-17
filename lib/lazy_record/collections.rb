3155c516a7c4be20ef77bfd23efa85b2e3f403ca
M. Simon Borg <msimonborg@gmail.com>
2017-04-17 18:59:31 -0400

    fix some rubocop issues

--- lib/lazy_record/collections.rb
+++ lib/lazy_record/collections.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'lazy_record/nesting'
 
 module LazyRecord
--- lib/lazy_record/nesting.rb
+++ lib/lazy_record/nesting.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module LazyRecord
   # Apply the same namespace nesting as self to another object.
   module Nesting
